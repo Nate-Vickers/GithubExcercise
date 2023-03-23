bd04efc9e35cd9434eda9ff933f65a3dd6d48a9d
Nate Vickers <npvickers@gmail.com>
2023-03-22 21:53:32 -0700

    Initial Commit

---
+++ GithubExcercise/GithubExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExcerciseApp.swift
+//  GithubExcercise
+//
+//  Created by Nate Vickers on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
