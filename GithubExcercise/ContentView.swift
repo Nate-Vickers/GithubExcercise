58ccb05a4a967538568be08d3fd02279b2590758
Nate Vickers <npvickers@gmail.com>
2023-03-28 17:40:18 -0700

    Version 3.0 - Blue

--- GithubExcercise/ContentView.swift
+++ GithubExcercise/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "2.circle")
+            Image(systemName: "3.circle")
                 .resizable()
                 .scaledToFit()
-                .foregroundColor(.green)
-            Text("Version 2.0")
+                .foregroundColor(.blue)
+            Text("Version 3.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundColor(.green)
+        .foregroundColor(.blue)
     }
 }
 
