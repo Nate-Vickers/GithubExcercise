df5cbc476ee9dbbc4ec190dfd69c6bdd0b01ae0a
Nate Vickers <npvickers@gmail.com>
2023-03-28 17:23:17 -0700

    Version 2.0 - Green 2 Circle

--- GithubExcercise/ContentView.swift
+++ GithubExcercise/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "1.circle")
+            Image(systemName: "2.circle")
                 .resizable()
                 .scaledToFit()
-                .foregroundColor(.red)
-            Text("Version 1.0")
+                .foregroundColor(.green)
+            Text("Version 2.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
         .padding()
-        .foregroundColor(.red)
+        .foregroundColor(.green)
     }
 }
 
